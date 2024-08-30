aded29578a2559229d20b18133bf4f9d2ca2f249
emilie-h <emilie.bressoud@heig-vd.ch>
2024-08-30 08:32:43 +0200

    fix: axios promise type

---
+++ src/auth/ProtectedRoutes.tsx
@@ -0,0 +1,20 @@
+import { Navigate } from "react-router-dom";
+import { FC, ReactNode } from "react";
+
+import { useAuth } from "@/auth/AuthProvider.tsx";
+
+interface Props {
+  children: ReactNode;
+}
+
+const ProtectedRoutes: FC<Props> = ({ children }) => {
+  const { isAuthenticated } = useAuth();
+
+  return (
+    <>
+      {isAuthenticated ? children : <Navigate replace to="/frontend/login" />}
+    </>
+  );
+};
+
+export default ProtectedRoutes;
